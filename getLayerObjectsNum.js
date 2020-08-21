01dd433ad64576d0412cd2948b88040cf230d009
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-21 09:08:20 +0500

    Delete getLayerObjectsNum.js

--- getLayerObjectsNum.js
+++
@@ -1,9 +0,0 @@
-// variables
-let layerObjectsNumArr = [];
-let layerObjectsNumFlag = false;
-// main
-window.addEventListener('load', function () {
-    let types = [];
-    for (let i = 0; i < map.layers.length; i++) {
-    }
-}, { once: true });
