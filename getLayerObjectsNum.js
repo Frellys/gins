50336e3feed18f825720bf897bfb8e330982349f
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-21 09:07:00 +0500

    Add files via upload

---
+++ getLayerObjectsNum.js
@@ -0,0 +1,9 @@
+// variables
+let layerObjectsNumArr = [];
+let layerObjectsNumFlag = false;
+// main
+window.addEventListener('load', function () {
+    let types = [];
+    for (let i = 0; i < map.layers.length; i++) {
+    }
+}, { once: true });
