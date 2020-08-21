a47500182a2b1dff8b4619d9be83c793b2ed5d04
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-21 08:59:51 +0500

    Add files via upload

--- getLayerObjectsNum.js
+++ getLayerObjectsNum.js
@@ -4,37 +4,4 @@ let layerObjectsNumFlag = false;
 // main
 window.addEventListener('load', function () {
     let types = [];
-    for (let i = 0; i < map.layers.length; i++) {
-        if (map.layers[i].protocol && map.layers[i].protocol.featureType) {
-            types.push(map.layers[i].protocol.featureType);
-            layerObjectsNumArr.push({
-                name: map.layers[i].name,
-                type: map.layers[i].protocol.featureType
-            });
-        }
-    }
-    let xhttp = new XMLHttpRequest();
-    xhttp.open('POST', window.location.origin + '/Feature/getLayerObjectsNum', true);
-    xhttp.setRequestHeader('Content-type', 'application/json; charset=windows-1251');
-    xhttp.send(JSON.stringify({
-        types: types.join(',')
-    }));
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let num = this.responseText.split(',');
-            for (let i = 0; i < layerObjectsNumArr.length; i++) {
-                layerObjectsNumArr[i].num = num[i];
-            }
-            let labels = document.querySelectorAll('.dataLayersDiv label');
-            for (let i = 0; i < labels.length; i++) {
-                for (let j = 0; j < layerObjectsNumArr.length; j++) {
-                    if (labels[i].innerText == ' ' + layerObjectsNumArr[j].name) {
-                        labels[i].innerText = ' ' + layerObjectsNumArr[j].name + ' [' + layerObjectsNumArr[j].num + ']';
-                        break;
-                    }
-                }
-            }
-            layerObjectsNumFlag = true;
-        }
-    };
 }, { once: true });
