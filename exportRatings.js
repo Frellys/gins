76087f0725cb63227f09525fcd53c791c898c111
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-07 09:43:08 +0500

    Add files via upload

--- exportRatings.js
+++ exportRatings.js
@@ -5,20 +5,16 @@
         if (this.readyState == 4 && this.status == 200) {
             // raw html
             let html = (new DOMParser()).parseFromString(this.responseText, 'text/html').querySelector('html');
-            console.log(html);
             // create export style
             let exportStyle = html.querySelector('body > style');
             document.body.appendChild(exportStyle);
-            console.log(exportStyle);
             // create export window
             let exportWindow = html.querySelector('body > div#exportWindow');
             exportWindow.style.display = 'none';
             document.body.appendChild(exportWindow);
-            console.log(exportWindow);
             // create export script
             let exportScript = Function(html.querySelector('body > script').innerHTML);
             exportScript();
-            console.log(exportScript);
             // create list element
             let li = document.createElement('li');
             document.querySelector('ul.settings-menu > li:nth-child(2) > ul').appendChild(li);
