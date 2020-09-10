e0eac3d2d5e47cf41fe32468102ee5d76c375fe0
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-10 13:24:40 +0500

    Add files via upload

---
+++ DirectoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace get_SUIS_dir_volume
+{
+    class DirectoryController
+    {
+        DirectoryInfo Dir { get; }
+        public DirectoryController(string path)
+        {
+            Dir = new DirectoryInfo(path);
+            if (!Dir.Exists) throw new DirectoryNotFoundException();
+        }
+        public DirectoryInfo[] Folders(bool recursive = false)
+        {
+            var folders = Dir.GetDirectories("*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            return folders;
+        }
+    }
+}
