72aa7927a107c9731402f57de7a77e840170fe32
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-11 14:41:08 +0500

    Add files via upload

--- parse.stMatix.js
+++ parse.stMatix.js
@@ -1,4 +1,4 @@
-(function () {
+{
     let dt = new Date();
     let ts =
         (dt.getFullYear().toString()) +
@@ -8,17 +8,16 @@
         (dt.getMinutes().toString().length < 2 ? '0' + dt.getMinutes().toString() : dt.getMinutes().toString()) +
         (dt.getSeconds().toString().length < 2 ? '0' + dt.getSeconds().toString() : dt.getSeconds().toString());
     let csv = [];
-    let rows = document.querySelectorAll('form[name="form1"] > font > table:first-child > tbody > tr');
-    for (let r of rows) {
+    document.querySelectorAll('form[name="form1"] > font > table:first-child > tbody > tr').forEach(function (r) {
         let csvStr = [];
         for (let c = 1; c < r.children.length - 1; c++) {
             csvStr.push('"' + r.children[c].innerText.replace(/(\r\n|\n|\r)/gm, '') + '"');
         }
         csv.push(csvStr.join(','));
-    }
+    });
     let csvFile = window.btoa(unescape(encodeURIComponent(csv.join('\n'))));
     let link = document.createElement('a');
     link.href = 'data:text/csv;base64,' + csvFile;
     link.download = ts + '.csv';
     link.click();
-})();
+}
