57e7cb98304eaa95a7c45db54e653a47114c93ba
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-11 08:45:44 +0500

    Add files via upload

--- parse.SUIS_sites_table.js
+++ parse.SUIS_sites_table.js
@@ -1,12 +1,13 @@
-let sites = [];
-document.querySelectorAll('table')[0].querySelectorAll('tbody > tr').forEach(function (el, idx) {
-    if (idx != 0) {
+{
+    let sites = [];
+    document.querySelectorAll('table')[0].querySelectorAll('tbody > tr:not(:first-child)').forEach(function (el) {
         sites.push({
             link: el.querySelectorAll('td')[0].querySelector('a').innerHTML,
-            name: el.querySelectorAll('td')[1].innerHTML,
+            name: el.querySelectorAll('td')[1].innerHTML.replace(/  /g, ' ').replace(/&nbsp;/g, ''),
             dir: '',
             status: ''
         });
-    }
-});
-console.log(sites);
+    });
+    console.log(sites);
+    copy(sites);
+}
