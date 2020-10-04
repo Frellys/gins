ed316b76e6da24f911dbca62d8ff9d37286b9d6b
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-04 09:14:52 +0000

    Add files via upload

--- KeyboardController.cs
+++ KeyboardController.cs
@@ -35,6 +35,38 @@ namespace torMacro
             { "KEY_K", 0x4B },
             { "KEY_T", 0x54 },
             { "KEY_V", 0x56 },
+            // functrion keys
+            { "VK_F1", 0x70 },
+            { "VK_F2", 0x71 },
+            { "VK_F3", 0x72 },
+            { "VK_F4", 0x73 },
+            { "VK_F5", 0x74 },
+            { "VK_F6", 0x75 },
+            { "VK_F7", 0x76 },
+            { "VK_F8", 0x77 },
+            { "VK_F9", 0x78 },
+            { "VK_F10", 0x79 },
+            { "VK_F11", 0x7A },
+            { "VK_F12", 0x7B },
+            { "VK_F13", 0x7C },
+            { "VK_F14", 0x7D },
+            { "VK_F15", 0x7E },
+            { "VK_F16", 0x7F },
+            { "VK_F17", 0x80 },
+            { "VK_F18", 0x81 },
+            { "VK_F19", 0x82 },
+            { "VK_F20", 0x83 },
+            { "VK_F21", 0x84 },
+            { "VK_F22", 0x85 },
+            { "VK_F23", 0x86 },
+            { "VK_F24", 0x87 },
+            // specials
+            { "VK_ESCAPE", 0x1B },
+            { "VK_LCONTROL", 0xA2 },
+            { "VK_LSHIFT", 0xA0 },
+            { "VK_LWIN", 0x5B },
+            { "VK_RETURN", 0x0D },
+            { "VK_TAB", 0x09 }
         };
         // arrows
         //public const byte VK_LEFT = 0x25;
@@ -57,33 +89,33 @@ namespace torMacro
         //public const byte KEY_T = 0x54;
         //public const byte KEY_V = 0x56;
         // functrion keys
-        public const byte VK_F1 = 0x70;
-        public const byte VK_F2 = 0x71;
-        public const byte VK_F3 = 0x72;
-        public const byte VK_F4 = 0x73;
-        public const byte VK_F5 = 0x74;
-        public const byte VK_F6 = 0x75;
-        public const byte VK_F7 = 0x76;
-        public const byte VK_F8 = 0x77;
-        public const byte VK_F9 = 0x78;
-        public const byte VK_F10 = 0x79;
-        public const byte VK_F11 = 0x7A;
-        public const byte VK_F12 = 0x7B;
-        public const byte VK_F13 = 0x7C;
-        public const byte VK_F14 = 0x7D;
-        public const byte VK_F15 = 0x7E;
-        public const byte VK_F16 = 0x7F;
-        public const byte VK_F17 = 0x80;
-        public const byte VK_F18 = 0x81;
-        public const byte VK_F19 = 0x82;
-        public const byte VK_F20 = 0x83;
-        public const byte VK_F21 = 0x84;
-        public const byte VK_F22 = 0x85;
-        public const byte VK_F23 = 0x86;
-        public const byte VK_F24 = 0x87;
+        //public const byte VK_F1 = 0x70;
+        //public const byte VK_F2 = 0x71;
+        //public const byte VK_F3 = 0x72;
+        //public const byte VK_F4 = 0x73;
+        //public const byte VK_F5 = 0x74;
+        //public const byte VK_F6 = 0x75;
+        //public const byte VK_F7 = 0x76;
+        //public const byte VK_F8 = 0x77;
+        //public const byte VK_F9 = 0x78;
+        //public const byte VK_F10 = 0x79;
+        //public const byte VK_F11 = 0x7A;
+        //public const byte VK_F12 = 0x7B;
+        //public const byte VK_F13 = 0x7C;
+        //public const byte VK_F14 = 0x7D;
+        //public const byte VK_F15 = 0x7E;
+        //public const byte VK_F16 = 0x7F;
+        //public const byte VK_F17 = 0x80;
+        //public const byte VK_F18 = 0x81;
+        //public const byte VK_F19 = 0x82;
+        //public const byte VK_F20 = 0x83;
+        //public const byte VK_F21 = 0x84;
+        //public const byte VK_F22 = 0x85;
+        //public const byte VK_F23 = 0x86;
+        //public const byte VK_F24 = 0x87;
         // specials
-        public const byte VK_ESCAPE = 0x1B;
-        public const byte VK_LCONTROL = 0xA2;
+        //public const byte VK_ESCAPE = 0x1B;
+        //public const byte VK_LCONTROL = 0xA2;
         public const byte VK_LSHIFT = 0xA0;
         public const byte VK_LWIN = 0x5B;
         public const byte VK_RETURN = 0x0D;
