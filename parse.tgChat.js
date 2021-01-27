7362933873d43fb78c8374a369e575cd9951285e
Frellys <50921387+Frellys@users.noreply.github.com>
2021-01-27 17:08:16 +0500

    Add files via upload

--- parse.tgChat.js
+++ parse.tgChat.js
@@ -3,7 +3,7 @@
     document.querySelectorAll('div.message.default').forEach(function (message) {
         let ts = message.querySelector('div.body > div.pull_right').getAttribute('title');
         let text = message.querySelector('div.body > div.text');
-        let link = text.querySelectorAll('a')[0].href;
+        let link = new URL(text.querySelectorAll('a')[0].href).hostname;
         let name = Array.from(text.childNodes).filter(function (t) { return (t.nodeName == '#text' && t.data.includes('Название организации:')); })[0].data.split(': ').pop();
         let stat = text.querySelectorAll('a[href=""]')[0].innerText;
         let prim = Array.from(text.childNodes).filter(function (t) { return (t.nodeName == '#text'); }).map(function (t) { return t.data; }).join(' ').split('Прим')[1].split(': ')[1].split(' Инстр')[0];
