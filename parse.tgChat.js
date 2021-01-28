d5b6a9885e0b66c65a651b5d3236ddb4c879b18c
Frellys <50921387+Frellys@users.noreply.github.com>
2021-01-28 16:28:28 +0500

    Add files via upload

--- parse.tgChat.js
+++ parse.tgChat.js
@@ -1,14 +1,16 @@
 (function () {
-    let ret = [['Time', 'Link', 'Name', 'Status', 'Comment', 'ID'].join('\t')];
+    let iis_sites = JSON.parse(jsonstr);
+    let ret = [['Time', 'Link', 'Name', 'Status', 'Comment', 'ID', 'IsOurs'].join('\t')];
     document.querySelectorAll('div.message.default').forEach(function (message) {
         let ts = message.querySelector('div.body > div.pull_right').getAttribute('title');
         let text = message.querySelector('div.body > div.text');
         let link = new URL(text.querySelectorAll('a')[0].href).hostname;
         let name = Array.from(text.childNodes).filter(function (t) { return (t.nodeName == '#text' && t.data.includes('Название организации:')); })[0].data.split(': ').pop();
         let stat = text.querySelectorAll('a[href=""]')[0].innerText;
-        let prim = Array.from(text.childNodes).filter(function (t) { return (t.nodeName == '#text'); }).map(function (t) { return t.data; }).join(' ').split('Прим')[1].split(': ')[1].split(' Инстр')[0];
+        let prim = ((stat == '#ВЫПОЛНЕНО') ? '' : Array.from(text.childNodes).filter(function (t) { return (t.nodeName == '#text'); }).map(function (t) { return t.data; }).join(' ').split('Прим')[1].split(': ')[1].split(' Инстр')[0]);
         let id = Array.from(text.querySelectorAll('a')).pop().innerText;
-        ret.push([ts, link, name, stat, prim, id].join('\t'));
+        let isOurs = iis_sites.includes(link).toString();
+        ret.push([ts, link, name, stat, prim, id, isOurs].join('\t'));
     });
     copy(ret.join('\n'));
 })();
