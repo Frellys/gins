945cb76f90d6d831ce6726c7cc7a152e7a317072
Frellys <50921387+Frellys@users.noreply.github.com>
2021-01-29 10:48:54 +0500

    Add files via upload

--- sw-test.js
+++ sw-test.js
@@ -1,10 +1,10 @@
 if ('serviceWorker' in navigator) {
     if (!navigator.serviceWorker.controller) {
-        navigator.serviceWorker.register('sw.js').then(() => {
+        navigator.serviceWorker.register('sw.js').then(function () {
             console.log('page: register register succeeded');
         });
-        navigator.serviceWorker.ready.then(registration => {
-            registration.active.postMessage('msg to newly registered sw');
+        navigator.serviceWorker.ready.then(function (swr) {
+            swr.active.postMessage('msg to newly registered sw');
         });
     }
     else {
