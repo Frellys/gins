02c24bd7fd954bb50feb044c560c13ea0eb42584
Frellys <50921387+Frellys@users.noreply.github.com>
2021-01-29 10:48:19 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
 self.addEventListener('install', function (e) {
     console.log('sw-evt: install');
     console.log(e);
+    e.waitUntil(caches.open('v1').then(function (cache) {
+        return cache.addAll([
+            '/favicon.ico'
+        ]);
+    }));
 }, false);
 
 self.addEventListener('fetch', function (e) {
     console.log(e);
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 }, false);
 
 self.addEventListener('message', function (msg) {
