b9636aa13e9cbcaf7ce8b7af61a62c6a94a21600
Frellys <50921387+Frellys@users.noreply.github.com>
2021-01-29 13:44:51 +0500

    Add files via upload

--- checkCapsLock.js
+++ checkCapsLock.js
@@ -1,32 +1,37 @@
-﻿// FIELD
-let inpPswdField = document.getElementById('Password');
-// IMG
-let warningImg = document.createElement('img');
-warningImg.src = location.origin + '/Scripts/SAS/img/SAS_warning.png';
-warningImg.style.height = document.getElementById('Password').offsetHeight + 'px';
-warningImg.style.verticalAlign = 'text-bottom';
-// WARNING LABEL
-let warningLabel = document.createElement('div');
-warningLabel.id = 'warningCL';
-warningLabel.style.position = 'absolute';
-warningLabel.style.textAlign = 'center';
-warningLabel.style.color = 'white';
-warningLabel.style.font = 'normal 1.6vmin Georgia';
-warningLabel.style.backgroundColor = 'rgba(50, 50, 50, 0.9)';
-// CHECK IF CAPSLOCK ENABLED ON PASSWORD FIELD
-inpPswdField.addEventListener('keyup', function (e) {
-    if (e.getModifierState('CapsLock')) {
-        if (!document.getElementById('warningCL')) {
-            warningLabel.style.top = inpPswdField.getBoundingClientRect().top + inpPswdField.offsetHeight + 'px';
-            warningLabel.style.left = inpPswdField.getBoundingClientRect().left + 'px';
-            warningLabel.style.width = inpPswdField.offsetWidth + 'px';
-            warningLabel.innerText = 'CapsLock';
-            // assign
-            warningLabel.prepend(warningImg);
-            document.body.appendChild(warningLabel);
-        }
+﻿window.addEventListener('DOMContentLoaded', function () {
+    // fields
+    let inpNameField = document.getElementById('LoginOrEmail');
+    let inpPswdField = document.getElementById('Password');
+    // wrap
+    let warningWrap = document.createElement('div');
+    warningWrap.id = 'warningWrap';
+    warningWrap.style.display = 'none';
+    warningWrap.flexDirection = 'row';
+    warningWrap.style.alignItems = 'flex-end';
+    warningWrap.style.width = 'fit-content';
+    warningWrap.style.margin = '0 auto';
+    document.getElementById('login-form-body').prepend(warningWrap);
+    // image
+    let warningImg = document.createElement('img');
+    warningImg.src = location.origin + '/Scripts/SAS/img/SAS_warning.png';
+    warningImg.style.height = '1em';
+    warningWrap.appendChild(warningImg);
+    // label
+    let warningLabel = document.createElement('span');
+    warningLabel.innerHTML = 'capslock enabled';
+    warningLabel.style.color = 'whitesmoke';
+    warningLabel.style.font = 'normal 1em monospace';
+    warningWrap.appendChild(warningLabel);
+    /**
+     * Checks for CapsLock on input event
+     * Sets 'display' property for warningWrap
+     * @param {Event} e
+     * @returns {void}
+     */
+    function checkModifierCapsState(e) {
+        document.getElementById('warningWrap').style.display = e.getModifierState('CapsLock') ? 'flex' : 'none';
     }
-    else {
-        if (document.getElementById('warningCL')) document.getElementById('warningCL').remove();
-    }
-}, false);
+    // set listeners
+    inpNameField.addEventListener('keyup', checkModifierCapsState, false);
+    inpPswdField.addEventListener('keyup', checkModifierCapsState, false);
+}, { once: true });
