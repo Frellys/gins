b450639864e362945c2764534d21a1a4fe650c24
Frellys <50921387+Frellys@users.noreply.github.com>
2021-03-04 11:50:56 +0500

    Add files via upload

--- pwa-test-sw.js
+++ pwa-test-sw.js
@@ -1,4 +1,14 @@
 console.log('sww');
 self.addEventListener('install', function (e) {
     console.log('evt-install');
+    e.waitUntil(caches.open('v1').then(function (cache) {
+        return cache.addAll([
+            '/pwa-test-icon.png'
+        ]);
+    }));
+});
+self.addEventListener('fetch', function (e) {
+    e.respondWith(caches.match(e.request).then((response) => {
+        return response || fetch(e.request);
+    }));
 });
