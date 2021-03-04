f0d38e15db7d8993ca882765124cc69983ce76e5
Frellys <50921387+Frellys@users.noreply.github.com>
2021-03-04 10:17:34 +0500

    Add files via upload

--- SUIS_override.js
+++ SUIS_override.js
@@ -3,4 +3,19 @@ window.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('div.content table').forEach(function (table) {
         table.style.width = 'auto';
     });
+    // remove static size from images
+    document.querySelectorAll('div.content img').forEach(function (img) {
+        img.style.width = 'auto';
+        img.style.height = 'auto';
+    });
+    // set horizontal bottom banners
+    if (window.location.hostname == 'kurchatov74.ru') {
+        document.querySelectorAll('div.page > div.cnt-wrp.bannersnet').forEach(function (wrap) {
+            wrap.style.display = 'flex';
+            wrap.style.gap = '1vmin';
+            wrap.querySelectorAll('div.br > a > img').forEach(function (img) {
+                img.style.maxWidth = 'unset';
+            });
+        });
+    }
 }, { once: true });
