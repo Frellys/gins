8ce911da36be5a607a65f4fe6352483cbf302cb9
Frellys <50921387+Frellys@users.noreply.github.com>
2021-03-11 17:02:54 +0500

    Add files via upload

---
+++ mp.js
@@ -0,0 +1,10 @@
+﻿let mpStates = [
+    'DD MM SS s',
+    'DD dddddd',
+    'DD MM mmm'
+];
+window.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('div#mp').addEventListener('click', function () {
+        mpStates.push(mpStates.shift());
+    }, false);
+}, { once: true });
