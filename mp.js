8d65b65c0bbe114498f5afb37c8ab4c42ced8221
Frellys <50921387+Frellys@users.noreply.github.com>
2021-03-12 15:20:38 +0500

    Add files via upload

--- mp.js
+++ mp.js
@@ -4,6 +4,11 @@
     'DD MM mmm'
 ];
 window.addEventListener('DOMContentLoaded', function () {
+    // display container on first mousemove
+    document.querySelector('div#map_container').addEventListener('mousemove', function () {
+        document.getElementById('mp').style.display = 'block';
+    }, { once: true });
+    // switch mpStates when container is clicked
     document.querySelector('div#mp').addEventListener('click', function () {
         mpStates.push(mpStates.shift());
     }, false);
