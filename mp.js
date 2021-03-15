600ccb5e9ae5de8b2bf6995e1c94837071fa7921
Frellys <50921387+Frellys@users.noreply.github.com>
2021-03-15 13:33:45 +0500

    Add files via upload

--- mp.js
+++ mp.js
@@ -1,15 +1,18 @@
-﻿let mpStates = [
-    'DD MM SS s',
-    'DD dddddd',
-    'DD MM mmm'
-];
+﻿let mpState = 'mp0';
 window.addEventListener('DOMContentLoaded', function () {
     // display container on first mousemove
-    document.querySelector('div#map_container').addEventListener('mousemove', function () {
-        document.getElementById('mp').style.display = 'block';
+    document.querySelector('#map_container').addEventListener('mousemove', function () {
+        document.querySelector('#mp_wrap').style.display = 'block';
     }, { once: true });
-    // switch mpStates when container is clicked
-    document.querySelector('div#mp').addEventListener('click', function () {
-        mpStates.push(mpStates.shift());
+    // set .select display onclick
+    document.querySelector('#mp_wrap > .switch').addEventListener('click', function () {
+        let sel = document.querySelector('#mp_wrap > .select');
+        sel.style.display = (sel.style.display == 'block') ? 'none' : 'block';
     }, false);
+    // set .select input change handlers
+    document.querySelectorAll('#mp_wrap > .select > .row > input').forEach(function (inp) {
+        inp.addEventListener('change', function (e) {
+            mpState = e.target.id;
+        }, false);
+    });
 }, { once: true });
