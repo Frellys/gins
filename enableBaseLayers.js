ba35c7fb1913402815a8ca9b038acedd9b72c405
Frellys <50921387+Frellys@users.noreply.github.com>
2021-03-16 11:08:16 +0500

    Add files via upload

---
+++ enableBaseLayers.js
@@ -0,0 +1,5 @@
+﻿let enableBaseLayers = function () {
+    if (window.location.pathname == '/Map/48') {
+        layersList.filter((el) => { return el.protocol && [2249, 2250, 2251].includes(el.protocol.id); }).forEach((el) => { el.setVisibility(true); });
+    }
+};
