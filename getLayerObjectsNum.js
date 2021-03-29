0ba3cdb3a38ceb01e8aa47c0c0c4b7353fc1643e
Frellys <50921387+Frellys@users.noreply.github.com>
2021-03-29 11:49:49 +0500

    Add files via upload

--- getLayerObjectsNum.js
+++ getLayerObjectsNum.js
@@ -1,12 +1,14 @@
-﻿window.addEventListener('load', function () {
+﻿let layerObjectsNumFlag = false;
+let layerObjects;
+window.addEventListener('load', function () {
     let layers = map.layers.filter(function (layer) { return ('features' in layer); });
-    let request = Array.from(document.querySelectorAll('.layersDiv > .dataLayersDiv > label')).map(function (label) {
-        let layer = layers.filter(function (layer) { return layer.name == label.innerHTML.trim(); })[0];
+    layerObjects = Array.from(document.querySelectorAll('.layersDiv > .dataLayersDiv > label')).map(function (label) {
+        let layer = layers.find(function (layer) { return layer.name == label.innerHTML.trim(); });
         if (layer) {
             return {
                 name: label.innerHTML.trim(),
                 table: layer.options.protocol.format.featureType,
-                layer: layer,
+                //layer: layer,
                 label: label
             };
         }
@@ -16,13 +18,18 @@
     xhr.setRequestHeader('Content-type', 'application/json');
     xhr.addEventListener('readystatechange', function () {
         if (this.readyState == 4 && this.status == 200) {
+            layerObjectsNumFlag = true;
             let nums = this.responseText.split(',');
-            request.forEach(function (req) {
-                req.label.innerHTML = '&nbsp;' + req.name + '&nbsp; [' + nums.shift() + ']';
+            layerObjects.forEach(function (req) {
+                req.num = nums.shift();
+                req.label.innerHTML = '&nbsp;' + req.name + '&nbsp;[' + req.num + ']';
+                delete req.table;
+                //delete req.layer;
+                delete req.label;
             });
         }
     }, false);
     xhr.send(JSON.stringify({
-        dbTables: request.map(function (el) { return el.table; })
+        dbTables: layerObjects.map(function (el) { return el.table; })
     }));
 }, { once: true });
