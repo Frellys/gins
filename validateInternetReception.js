ad7322f2e5abe005b71d68145ed98b280c938c3c
Frellys <50921387+Frellys@users.noreply.github.com>
2021-06-22 16:24:38 +0500

    Add files via upload

--- validateInternetReception.js
+++ validateInternetReception.js
@@ -1,56 +1,58 @@
 ﻿let reqFields;
 window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('form#addform').setAttribute('autocomplete', 'off');
-    reqFields = {
-        'Title': { text: '- Необходимо заполнить Ф.И.О.' },
-        'Email': { text: '- Необходимо заполнить E-mail' },
-        'Text': { text: '- Необходимо заполнить текст обращения'},
-        'Agree': { text: '- Необходимо согласиться на обработку персональных данных' },
-        'CaptchaInput': { text: '- Необходимо заполнить капчу' }
-    };
-    setSubmitTo('disabled');
-    updateSubmitRequirements();
-    Object.keys(reqFields).forEach(function (el) {
-        reqFields[el] = {
-            id: el,
-            text: reqFields[el].text,
-            isFilled: false
+    if (window.location.pathname.includes('InternetReception')) {
+        document.querySelector('form#addform').setAttribute('autocomplete', 'off');
+        reqFields = {
+            'Title': { text: '- Необходимо заполнить Ф.И.О.' },
+            'Email': { text: '- Необходимо заполнить E-mail' },
+            'Text': { text: '- Необходимо заполнить текст обращения' },
+            'Agree': { text: '- Необходимо согласиться на обработку персональных данных' },
+            'CaptchaInput': { text: '- Необходимо заполнить капчу' }
         };
-        document.getElementById(el).addEventListener('input', function () {
-            reqFields[this.id].isFilled = !!(this[(this.type == 'checkbox') ? 'checked' : 'value']);
-            updateSubmitRequirements();
+        setSubmitTo('disabled');
+        updateSubmitRequirements();
+        Object.keys(reqFields).forEach(function (el) {
+            reqFields[el] = {
+                id: el,
+                text: reqFields[el].text,
+                isFilled: false
+            };
+            document.getElementById(el).addEventListener('input', function () {
+                reqFields[this.id].isFilled = !!(this[(this.type == 'checkbox') ? 'checked' : 'value']);
+                updateSubmitRequirements();
+            }, false);
+        });
+        let ext = [
+            '.doc',
+            '.docx',
+            '.gif',
+            '.jpg',
+            '.jpeg',
+            '.odp',
+            '.ods',
+            '.odt',
+            '.pdf',
+            '.png',
+            '.pps',
+            '.ppsx',
+            '.ppt',
+            '.pptx',
+            '.rar',
+            '.rtf',
+            '.tif',
+            '.txt',
+            '.xls',
+            '.xlsx',
+            '.zip'
+        ];
+        document.querySelector('input#Attach_filebox').setAttribute('accept', ext.join(','));
+        document.querySelector('input#Attach_filebox').removeAttribute('multiple');
+        document.querySelector('input#Attach_filebox').addEventListener('input', function (e) {
+            if (e.target.files[0].type == '') {
+                this.value = '';
+            }
         }, false);
-    });
-    let ext = [
-        '.doc',
-        '.docx',
-        '.gif',
-        '.jpg',
-        '.jpeg',
-        '.odp',
-        '.ods',
-        '.odt',
-        '.pdf',
-        '.png',
-        '.pps',
-        '.ppsx',
-        '.ppt',
-        '.pptx',
-        '.rar',
-        '.rtf',
-        '.tif',
-        '.txt',
-        '.xls',
-        '.xlsx',
-        '.zip'
-    ];
-    document.querySelector('input#Attach_filebox').setAttribute('accept', ext.join(','));
-    document.querySelector('input#Attach_filebox').removeAttribute('multiple');
-    document.querySelector('input#Attach_filebox').addEventListener('input', function (e) {
-        if (e.target.files[0].type == '') {
-            this.value = '';
-        }
-    }, false);
+    }
 }, { once: true });
 
 function setSubmitTo(str) {
