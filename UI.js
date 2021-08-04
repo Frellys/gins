6b4f2b41602c1ed600f05c2ef6105f3cb301fc88
Frellys <50921387+Frellys@users.noreply.github.com>
2021-08-04 18:32:23 +0500

    Add files via upload

--- UI.js
+++ UI.js
@@ -6,7 +6,7 @@ let UI = new function () {
     this.activeSection = false;
     this.settings = {
         bgTemplate: true,
-        transition: 'all 0.3s linear',
+        transition: 'all 0.25s linear',
         style: {}
     };
     this.parts = [];
@@ -36,18 +36,18 @@ UI.explorer = new function () {
     this.div.style.width = '35rem';
     this.div.style.left = `calc(3rem - ${this.div.style.width})`;
     this.div.style.transition = UI.settings.transition;
-    window.addEventListener('keyup', function (e) {
-        if (e.code == 'Escape') {
-            if (UI.explorer.div.getAttribute('data-visible') == 'true') {
-                UI.explorer.div.style.left = `calc(3rem - ${UI.explorer.div.style.width})`;
-                UI.explorer.div.setAttribute('data-visible', false);
-            }
-            else {
-                UI.explorer.div.style.left = '3rem';
-                UI.explorer.div.setAttribute('data-visible', true);
-            }
-        }
-    }, false);
+    //window.addEventListener('keyup', function (e) {
+    //    if (e.code == 'Escape') {
+    //        if (UI.explorer.div.getAttribute('data-visible') == 'true') {
+    //            UI.explorer.div.style.left = `calc(3rem - ${UI.explorer.div.style.width})`;
+    //            UI.explorer.div.setAttribute('data-visible', false);
+    //        }
+    //        else {
+    //            UI.explorer.div.style.left = '3rem';
+    //            UI.explorer.div.setAttribute('data-visible', true);
+    //        }
+    //    }
+    //}, false);
 };
 
 UI.explorer.layers = new function () {
@@ -68,7 +68,7 @@ UI.explorer.layers = new function () {
                 checkbox.layer = layer;
                 checkbox.style.outline = 'none';
                 checkbox.oninput = function (e) {
-                    e.target.layer.setVisibility(e.target.checked);
+                    this.layer.setVisibility(this.checked);
                 };
                 wrap.appendChild(checkbox);
                 let label = document.createElement('label');
@@ -76,9 +76,55 @@ UI.explorer.layers = new function () {
                 label.setAttribute('for', checkbox.id);
                 wrap.appendChild(label);
                 UI.explorer.layers.list.appendChild(wrap);
+                // update
+                layer.refToNewExplorer = wrap;
             }
         });
     }, false);
+};
+
+UI.explorer.share = new function () {
+    this.div = UI.explorer.div.querySelector('.share');
+    this.div.style.display = 'none';
+    this.h1 = this.div.querySelector('h1');
+    this.Update = function () {
+        var zoom = map.zoom;
+        var lon = map.center.lon;
+        var lat = map.center.lat;
+        UI.explorer.share.link.textarea.value = 'http://' + window.location.host + window.location.pathname + '?zoom=' + zoom + '&lon=' + lon + '&lat=' + lat + '&foreignRequest=false' + getEnabledLayersQuery();
+        //UI.explorer.share.html.textarea.value = 'html';
+        var zoom = map.zoom;
+        var lon = map.center.lon;
+        var lat = map.center.lat;
+        var url = 'http://' + window.location.host + '/Map/ShowMap/' + modelId + '?zoom=' + zoom + '&lon=' + lon + '&lat=' + lat + '&foreignRequest=true';
+        UI.explorer.share.html.textarea.value = '<iframe width="600" height="600"src="' + url + getEnabledLayersQuery() + '"></iframe>';
+    };
+};
+
+UI.explorer.share.link = new function () {
+    this.div = UI.explorer.share.div.querySelector('.link-wrap');
+    this.h2 = this.div.querySelector('h2');
+    this.textarea = this.div.querySelector('textarea');
+    this.button = this.div.querySelector('button');
+    this.button.onclick = function () {
+        navigator.clipboard.writeText(UI.explorer.share.link.textarea.value);
+    };
+};
+
+UI.explorer.share.html = new function () {
+    this.div = UI.explorer.share.div.querySelector('.html-wrap');
+    this.h2 = this.div.querySelector('h2');
+    this.textarea = this.div.querySelector('textarea');
+    this.button = this.div.querySelector('button');
+    this.button.onclick = function () {
+        navigator.clipboard.writeText(UI.explorer.share.html.textarea.value);
+    };
+};
+
+UI.explorer.export = new function () {
+    this.div = UI.explorer.div.querySelector('.export');
+    this.div.style.display = 'none';
+    this.h1 = this.div.querySelector('h1');
 };
 
 UI.explorer.settings = new function () {
@@ -103,7 +149,8 @@ UI.sidebar = new function () {
 UI.sidebar.div.querySelectorAll('svg:not(#menu)').forEach(function (svg) {
     svg.onclick = function (e) {
         let cur = UI.explorer.div.getAttribute('data-current');
-        if (e.target.id == cur) {
+        console.log(this.id);
+        if (this.id == cur) {
             if (UI.explorer.div.getAttribute('data-visible') == 'true') {
                 UI.explorer.div.style.left = `calc(3rem - ${UI.explorer.div.style.width})`;
                 UI.explorer.div.setAttribute('data-visible', false);
@@ -124,6 +171,9 @@ UI.sidebar.div.querySelectorAll('svg:not(#menu)').forEach(function (svg) {
                 UI.explorer.div.setAttribute('data-visible', true);
             }
             UI.explorer.div.setAttribute('data-current', e.target.id);
+        }
+        if (this.id == 'share') {
+            UI.explorer.share.Update();
         }
     };
 });
