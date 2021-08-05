dfbedf7235e10aa5e87d1b5343620c8bccdedcdc
Frellys <50921387+Frellys@users.noreply.github.com>
2021-08-05 17:20:14 +0500

    Add files via upload

---
+++ gis-ui.js
@@ -0,0 +1,16 @@
+﻿let gisUI = document.querySelector('.gis-ui');
+gisUI.setAttribute('explorerIsVisible', false);
+gisUI.setAttribute('riserIsVisible', false);
+document.documentElement.style.setProperty('--gis-ui-explorer-width', '35vw');
+document.documentElement.style.setProperty('--gis-ui-riser-height', '35vh');
+
+window.addEventListener('keyup', function (e) {
+    if (e.code == 'Escape') {
+        gisUI.setAttribute('explorerIsVisible', (gisUI.getAttribute('explorerIsVisible') == 'false'));
+        map.updateSize();
+    }
+    if (e.code == 'F2') {
+        gisUI.setAttribute('riserIsVisible', (gisUI.getAttribute('riserIsVisible') == 'false'));
+        map.updateSize();
+    }
+}, false);
