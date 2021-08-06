df586cb67fd3179ce7eecdbf93d547fbef188bab
Frellys <50921387+Frellys@users.noreply.github.com>
2021-08-06 16:24:41 +0500

    Add files via upload

--- gis-ui.js
+++ gis-ui.js
@@ -1,8 +1,10 @@
 ﻿let gisUI = document.querySelector('.gis-ui');
 gisUI.setAttribute('explorerIsVisible', false);
 gisUI.setAttribute('riserIsVisible', false);
-document.documentElement.style.setProperty('--gis-ui-explorer-width', '35vw');
+gisUI.setAttribute('stateAlternate', false);
+document.documentElement.style.setProperty('--gis-ui-explorer-width', '25vw');
 document.documentElement.style.setProperty('--gis-ui-riser-height', '35vh');
+document.documentElement.style.setProperty('--gis-ui-resizer-thickness', '0.5rem');
 
 window.addEventListener('keyup', function (e) {
     if (e.code == 'Escape') {
@@ -13,4 +15,30 @@ window.addEventListener('keyup', function (e) {
         gisUI.setAttribute('riserIsVisible', (gisUI.getAttribute('riserIsVisible') == 'false'));
         map.updateSize();
     }
+}, false);
+
+window.addEventListener('keydown', handleAlternate, false);
+window.addEventListener('keyup', handleAlternate, false);
+
+function handleAlternate(e) {
+    console.log(e);
+    if (e.code == 'AltLeft') {
+        e.preventDefault();
+        switch (e.type) {
+            case (gisUI.getAttribute('stateAlternate') == 'false' && 'keydown'): {
+                gisUI.setAttribute('stateAlternate', true);
+                console.log(true);
+                break;
+            };
+            case (gisUI.getAttribute('stateAlternate') == 'true' && 'keyup'): {
+                gisUI.setAttribute('stateAlternate', false);
+                console.log(false);
+                break;
+            };
+        };
+    }
+};
+
+window.addEventListener('blur', function () {
+    gisUI.setAttribute('stateAlternate', false);
 }, false);
