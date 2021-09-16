94ee75dd6ac9e640f29b46412d4794eed3c14558
Frellys <50921387+Frellys@users.noreply.github.com>
2021-09-16 17:29:58 +0500

    Add files via upload

--- SUIS_CompressFiles.cs
+++ SUIS_CompressFiles.cs
@@ -40,82 +40,130 @@ namespace SUIS_CompressFiles
         /// </summary>
         public void Save()
         {
-            File.WriteAllLines(Path.Combine(AppContext.BaseDirectory, "log.txt"), arr, System.Text.Encoding.UTF8);
+            File.WriteAllLines(Path.Combine(AppContext.BaseDirectory, $"{DateTime.Now.Ticks}.txt"), arr, System.Text.Encoding.UTF8);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Dictionary<string, ImageFormat> iFormat = new Dictionary<string, ImageFormat>
+            {
+                { ".png", ImageFormat.Png },
+                { ".jpg", ImageFormat.Jpeg }
+            };
             Logger logger = new Logger();
             try
             {
+                long cap = 4 * (int)Math.Pow(2, 20);
                 string root = @"C:\Hosting\region74";
                 List<string> siteDirs = Directory.GetDirectories(root, "*", SearchOption.TopDirectoryOnly).Where(s => Directory.Exists(Path.Combine(s, "Upload"))).ToList();
                 foreach (string siteDir in siteDirs)
                 {
                     logger.Add(new string[] {
-                        "***   SITE: " + siteDir
+                        "***     SITE: " + siteDir
                     });
-                    string uploadDir = Path.Combine(siteDir, "Upload");
-                    string uploadFiles = Path.Combine(uploadDir, "files");
-                    string uploadImages = Path.Combine(uploadDir, "images");
-                    logger.Add(new string[] {
-                        "***  FILES: " + Directory.GetFiles(uploadFiles, "*", SearchOption.TopDirectoryOnly).Length.ToString(),
-                        "*** IMAGES: " + Directory.GetFiles(uploadImages, "*", SearchOption.TopDirectoryOnly).Length.ToString(),
-                        string.Empty
-                    });
-                    if (Directory.Exists(uploadFiles))
+                    string AppDataDir = Path.Combine(siteDir, "App_Data");
+                    if (Directory.Exists(AppDataDir))
                     {
-                        /*
-                         * maybe later
-                         */
-                    }
-                    if (Directory.Exists(uploadImages))
-                    {
-                        foreach (string image in Directory.GetFiles(uploadImages, "*", SearchOption.TopDirectoryOnly))
+                        foreach (string file in Directory.GetFiles(AppDataDir, "*", SearchOption.AllDirectories))
                         {
-                            long size = new FileInfo(image).Length;
-                            string ext = Path.GetExtension(image).ToLower();
-                            if (ext == ".png")
+                            long size = new FileInfo(file).Length;
+                            if (size > cap)
                             {
-                                Bitmap b = new Bitmap(image);
-                                ImageCodecInfo c = ImageCodecInfo.GetImageDecoders().ToList().Find(d => d.FormatID == ImageFormat.Png.Guid);
-                                EncoderParameters eps = new EncoderParameters(1);
-                                EncoderParameter ep = new EncoderParameter(Encoder.Quality, 50L);
-                                eps.Param[0] = ep;
-                                string nname = "_" + Path.GetFileName(image);
-                                b.Save(Path.Combine(uploadImages, nname), c, eps);
-                                b.Dispose();
+                                string ext = Path.GetExtension(file).ToLower();
+                                if (ext == ".jpg" || ext == ".png")
+                                {
+                                    // move original
+                                    string decomp = Path.Combine(Path.GetDirectoryName(file), "_decomp_" + Path.GetFileName(file));
+                                    Console.WriteLine(decomp);
+                                    File.Move(file, decomp);
+                                    // compress
+                                    Bitmap b = new Bitmap(decomp);
+                                    ImageCodecInfo c = ImageCodecInfo.GetImageDecoders().ToList().Find(d => d.FormatID == iFormat[ext].Guid);
+                                    EncoderParameters eps = new EncoderParameters(1);
+                                    EncoderParameter ep = new EncoderParameter(Encoder.Quality, 50L);
+                                    eps.Param[0] = ep;
+                                    b.Save(file, c, eps);
+                                    b.Dispose();
+                                    // delete old
+                                    File.Delete(decomp);
+                                    // log
+                                    logger.Add(new string[] {
+                                        file,
+                                        size.ToString()
+                                    });
+                                }
                             }
-                            if (ext == ".jpg")
-                            {
-                                Bitmap b = new Bitmap(image);
-                                ImageCodecInfo c = ImageCodecInfo.GetImageDecoders().ToList().Find(d => d.FormatID == ImageFormat.Jpeg.Guid);
-                                EncoderParameters eps = new EncoderParameters(1);
-                                EncoderParameter ep = new EncoderParameter(Encoder.Quality, 50L);
-                                eps.Param[0] = ep;
-                                string nname = "_" + Path.GetFileName(image);
-                                b.Save(Path.Combine(uploadImages, nname), c, eps);
-                                b.Dispose();
-                            }
-                            logger.Add(new string[] {
-                                image,
-                                size.ToString(),
-                                string.Empty
-                            });
                         }
                     }
+                    else
+                    {
+                        logger.Add(new string[] { "*** APP_DATA: NO FOLDER" });
+                    }
+                    string uploadDir = Path.Combine(siteDir, "Upload");
+                    if (Directory.Exists(uploadDir))
+                    {
+                        string uploadFiles = Path.Combine(uploadDir, "files");
+                        string uploadImages = Path.Combine(uploadDir, "images");
+                        if (Directory.Exists(uploadFiles))
+                        {
+                            /*
+                             * maybe later
+                             */
+                        }
+                        else
+                        {
+                            logger.Add(new string[] { "***    FILES: NO FOLDER" });
+                        }
+                        if (Directory.Exists(uploadImages))
+                        {
+                            foreach (string image in Directory.GetFiles(uploadImages, "*", SearchOption.TopDirectoryOnly))
+                            {
+                                long size = new FileInfo(image).Length;
+                                if (size > cap)
+                                {
+                                    string ext = Path.GetExtension(image).ToLower();
+                                    if (ext == ".jpg" || ext == ".png")
+                                    {
+                                        // move original
+                                        string decomp = Path.Combine(uploadImages, "_decomp_" + Path.GetFileName(image));
+                                        File.Move(image, decomp);
+                                        // compress
+                                        Bitmap b = new Bitmap(decomp);
+                                        ImageCodecInfo c = ImageCodecInfo.GetImageDecoders().ToList().Find(d => d.FormatID == iFormat[ext].Guid);
+                                        EncoderParameters eps = new EncoderParameters(1);
+                                        EncoderParameter ep = new EncoderParameter(Encoder.Quality, 50L);
+                                        eps.Param[0] = ep;
+                                        b.Save(image, c, eps);
+                                        b.Dispose();
+                                        // delete old
+                                        File.Delete(decomp);
+                                        // log
+                                        logger.Add(new string[] {
+                                            image,
+                                            size.ToString()
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            logger.Add(new string[] { "***   IMAGES: NO FOLDER" });
+                        }
+                    }
+                    logger.Add(new string[] { string.Empty });
                     // temp
-                    break;
+                    // break;
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            finally {
-                // save logs
+            finally
+            {
                 logger.Save();
             }
             // await
