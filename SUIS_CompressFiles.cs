cd013f0534910b316a083d932bd258731ce6ad21
Frellys <50921387+Frellys@users.noreply.github.com>
2021-09-17 19:33:21 +0500

    Add files via upload

--- SUIS_CompressFiles.cs
+++ SUIS_CompressFiles.cs
@@ -61,99 +61,36 @@ namespace SUIS_CompressFiles
                 foreach (string siteDir in siteDirs)
                 {
                     logger.Add(new string[] {
-                        "***     SITE: " + siteDir
+                        "*** SITE: " + siteDir
                     });
-                    string AppDataDir = Path.Combine(siteDir, "App_Data");
-                    if (Directory.Exists(AppDataDir))
+                    foreach (string file in Directory.GetFiles(siteDir, "*", SearchOption.AllDirectories))
                     {
-                        foreach (string file in Directory.GetFiles(AppDataDir, "*", SearchOption.AllDirectories))
+                        long size = new FileInfo(file).Length;
+                        string ext = Path.GetExtension(file).ToLower();
+                        if ((ext == ".jpg" || ext == ".png") && size > cap)
                         {
-                            long size = new FileInfo(file).Length;
-                            if (size > cap)
-                            {
-                                string ext = Path.GetExtension(file).ToLower();
-                                if (ext == ".jpg" || ext == ".png")
-                                {
-                                    // move original
-                                    string decomp = Path.Combine(Path.GetDirectoryName(file), "_decomp_" + Path.GetFileName(file));
-                                    Console.WriteLine(decomp);
-                                    File.Move(file, decomp);
-                                    // compress
-                                    Bitmap b = new Bitmap(decomp);
-                                    ImageCodecInfo c = ImageCodecInfo.GetImageDecoders().ToList().Find(d => d.FormatID == iFormat[ext].Guid);
-                                    EncoderParameters eps = new EncoderParameters(1);
-                                    EncoderParameter ep = new EncoderParameter(Encoder.Quality, 50L);
-                                    eps.Param[0] = ep;
-                                    b.Save(file, c, eps);
-                                    b.Dispose();
-                                    // delete old
-                                    File.Delete(decomp);
-                                    // log
-                                    logger.Add(new string[] {
-                                        file,
-                                        size.ToString()
-                                    });
-                                }
-                            }
+                            // move original
+                            string decomp = Path.Combine(Path.GetDirectoryName(file), "_decomp_" + Path.GetFileName(file));
+                            Console.WriteLine(decomp);
+                            File.Move(file, decomp);
+                            // compress
+                            Bitmap b = new Bitmap(decomp);
+                            ImageCodecInfo c = ImageCodecInfo.GetImageDecoders().ToList().Find(d => d.FormatID == iFormat[ext].Guid);
+                            EncoderParameters eps = new EncoderParameters(1);
+                            EncoderParameter ep = new EncoderParameter(Encoder.Quality, 50L);
+                            eps.Param[0] = ep;
+                            b.Save(file, c, eps);
+                            b.Dispose();
+                            // delete old
+                            File.Delete(decomp);
+                            // log
+                            //logger.Add(new string[] {
+                            //    file,
+                            //    size.ToString()
+                            //});
                         }
                     }
-                    else
-                    {
-                        logger.Add(new string[] { "*** APP_DATA: NO FOLDER" });
-                    }
-                    string uploadDir = Path.Combine(siteDir, "Upload");
-                    if (Directory.Exists(uploadDir))
-                    {
-                        string uploadFiles = Path.Combine(uploadDir, "files");
-                        string uploadImages = Path.Combine(uploadDir, "images");
-                        if (Directory.Exists(uploadFiles))
-                        {
-                            /*
-                             * maybe later
-                             */
-                        }
-                        else
-                        {
-                            logger.Add(new string[] { "***    FILES: NO FOLDER" });
-                        }
-                        if (Directory.Exists(uploadImages))
-                        {
-                            foreach (string image in Directory.GetFiles(uploadImages, "*", SearchOption.TopDirectoryOnly))
-                            {
-                                long size = new FileInfo(image).Length;
-                                if (size > cap)
-                                {
-                                    string ext = Path.GetExtension(image).ToLower();
-                                    if (ext == ".jpg" || ext == ".png")
-                                    {
-                                        // move original
-                                        string decomp = Path.Combine(uploadImages, "_decomp_" + Path.GetFileName(image));
-                                        File.Move(image, decomp);
-                                        // compress
-                                        Bitmap b = new Bitmap(decomp);
-                                        ImageCodecInfo c = ImageCodecInfo.GetImageDecoders().ToList().Find(d => d.FormatID == iFormat[ext].Guid);
-                                        EncoderParameters eps = new EncoderParameters(1);
-                                        EncoderParameter ep = new EncoderParameter(Encoder.Quality, 50L);
-                                        eps.Param[0] = ep;
-                                        b.Save(image, c, eps);
-                                        b.Dispose();
-                                        // delete old
-                                        File.Delete(decomp);
-                                        // log
-                                        logger.Add(new string[] {
-                                            image,
-                                            size.ToString()
-                                        });
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            logger.Add(new string[] { "***   IMAGES: NO FOLDER" });
-                        }
-                    }
-                    logger.Add(new string[] { string.Empty });
+                    //logger.Add(new string[] { string.Empty });
                     // temp
                     // break;
                 }
