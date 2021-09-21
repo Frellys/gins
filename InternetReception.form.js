bd3297806598da48fe7be3045759d4e64034de9e
Frellys <50921387+Frellys@users.noreply.github.com>
2021-09-21 17:26:30 +0500

    Add files via upload

---
+++ InternetReception.form.js
@@ -0,0 +1,6 @@
+﻿window.addEventListener('load', function () {
+    let main = document.body.querySelector('main');
+    let params = Object.fromEntries(window.location.search.slice(1).split('&').map(p => p.split('=')));
+    let form = document.body.querySelector(`#${params['form']}`).content.cloneNode(true);
+    main.appendChild(form);
+}, false);
