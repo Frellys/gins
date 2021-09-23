fe003f0fa5fab1de1d1caf2ea938ee8921c1dfaa
Frellys <50921387+Frellys@users.noreply.github.com>
2021-09-23 17:14:18 +0500

    Add files via upload

--- InternetReception.form.js
+++ InternetReception.form.js
@@ -2,23 +2,19 @@
     if (window.location.pathname === '/') {
         window.location.pathname = '/Home';
     }
-    if (window.location.search === '') {
+    if (window.location.search.includes('form=') === false) {
         window.location.search = '?form=ombudsman';
     }
     let main = document.body.querySelector('main');
     let params = Object.fromEntries(window.location.search.slice(1).split('&').map(p => p.split('=')));
-    let form = document.body.querySelector(`#${params['form']}`).content.cloneNode(true);
-    main.appendChild(form);
-    document.body.querySelector('#submit').addEventListener('click', function () {
-        if (this.classList.contains('disabled')) {
-            let xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    console.log(this.response);
-                }
-            };
-            xhr.open('POST', '/Home/SendMail', true);
-            xhr.send();
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let doc = new DOMParser().parseFromString(this.response, 'text/html');
+            main.appendChild(doc.body.querySelector('form'));
+            document.body.querySelector('#submit').addEventListener('click', sendMail, false);
         }
-    }, false);
+    };
+    xhr.open('GET', `/form/${params['form']}.html`, true);
+    xhr.send();
 }, false);
