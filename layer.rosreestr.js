e213a32bb342c16a1e5a2b5b8a53d483263258f7
Frellys <50921387+Frellys@users.noreply.github.com>
2021-11-25 18:52:42 +0500

    Add files via upload

--- layer.rosreestr.js
+++ layer.rosreestr.js
@@ -3,9 +3,8 @@
     { name: 'Здания (браунфилд)', marker: '/Scripts/SAS/img/place-marker-64-gray.png' },
     { name: 'Индустриальные парки', marker: '/Scripts/SAS/img/place-marker-64-brown.png' },
     { name: 'Росреестр рекреация', marker: '/Scripts/SAS/img/place-marker-64-red.png' },
-    //'Здания (браунфилд)',
-    //'Индустриальные парки',
-    //'Росреестр рекреация'
+    // ours
+    { name: 'Мостовые сооружения', marker: '/Scripts/SAS/img/place-marker-64-yellow.png' }
 ];
 window.addEventListener('DOMContentLoaded', function () {
     map.layers.forEach(function (l) {
@@ -15,56 +14,63 @@ window.addEventListener('DOMContentLoaded', function () {
             l.markers.setZIndex(1001);
             l.markers.setVisibility(true);
             l.events.register('loadend', l, function () {
-                let xhr = new XMLHttpRequest();
-                xhr.open('GET', `/Scripts/SAS/pkg/${l.name}.txt`, true);
-                xhr.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        let res = JSON.parse(this.response);
-                        let features = res.features;
-                        features.forEach(function (f) {
-                            let geometry = new OpenLayers.Geometry.MultiPolygon(f.geometry.coordinates.map(function (c) {
-                                return new OpenLayers.Geometry.Polygon(c.map(function (a) {
-                                    return new OpenLayers.Geometry.LinearRing(a.map(function (p) {
-                                        return new OpenLayers.Geometry.Point(p[0], p[1]).transform(new OpenLayers.Projection('EPSG:4326'), new OpenLayers.Projection('EPSG:900913'));
-                                    }));
-                                }));
-                            }));
-                            let feature = new OpenLayers.Feature.Vector(geometry, null, clone(l.styleMap.styles.default.defaultStyle));
-                            feature.props = f.properties;
-                            l.addFeatures([feature]);
-                            let point = new OpenLayers.LonLat(feature.props.center.x, feature.props.center.y).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
-                            //let icon = new OpenLayers.Icon('/Scripts/SAS/img/place-marker-64.png', new OpenLayers.Size(24, 24), new OpenLayers.Pixel(-12, -12));
-                            let icon = new OpenLayers.Icon(rosreestr_layers.find(rl => rl.name === l.name).marker, new OpenLayers.Size(24, 24), new OpenLayers.Pixel(-12, -12));
-                            let marker = new OpenLayers.Marker(point, icon);
-                            marker.feature = feature;
-                            marker.events.register('click', marker, function (e) {
-                                map.controls.find(c => c.displayClass === 'olControlSelectFeature').select(e.object.feature);
-                            });
-                            l.markers.addMarker(marker, icon);
-                        });
-                        l.markers.redraw();
-                    }
-                };
-                xhr.send();
+                l.features.forEach(function (feature) {
+                    let g = feature.geometry.components[0].components[0].components;
+                    let center = new function () {
+                        this.x = g.reduce(function (acc, cur) { return acc + cur.x; }, 0) / g.length;
+                        this.y = g.reduce(function (acc, cur) { return acc + cur.y; }, 0) / g.length;
+                    };
+                    let point = new OpenLayers.LonLat(center.x, center.y);
+                    let icon = new OpenLayers.Icon(rosreestr_layers.find(rl => rl.name === l.name).marker, new OpenLayers.Size(24, 24), new OpenLayers.Pixel(-12, -12));
+                    let marker = new OpenLayers.Marker(point, icon);
+                    marker.feature = feature;
+                    marker.events.register('click', marker, function (e) {
+                        map.controls.find(c => c.displayClass === 'olControlSelectFeature').select(e.object.feature);
+                    });
+                    l.markers.addMarker(marker, icon);
+                });
+                l.markers.redraw();
             });
         }
     });
+    // set toser
+    let toser = map.layers.find(l => l.name === 'ТОСЭР');
+    toser.markers = new OpenLayers.Layer.Markers('markers');
+    map.addLayer(toser.markers);
+    toser.markers.setZIndex(1001);
+    toser.markers.setVisibility(true);
+    toser.events.register('loadend', toser, function () {
+        toser.features.forEach(function (f) {
+            let g = f.geometry.components[0].components[0].components;
+            let center = new function () {
+                this.x = g.reduce(function (acc, cur) { return acc + cur.x; }, 0) / g.length;
+                this.y = g.reduce(function (acc, cur) { return acc + cur.y; }, 0) / g.length;
+            };
+            let point = new OpenLayers.LonLat(center.x, center.y);
+            let icon = new OpenLayers.Icon('/Scripts/SAS/img/place-marker-64-red.png', new OpenLayers.Size(24, 24), new OpenLayers.Pixel(-12, -12));
+            let marker = new OpenLayers.Marker(point, icon);
+            marker.feature = f;
+            marker.events.register('click', marker, function (e) {
+                map.controls.find(c => c.displayClass === 'olControlSelectFeature').select(e.object.feature);
+            });
+            toser.markers.addMarker(marker, icon);
+        });
+    });
 }, false);
-let rosreestr_outer = [
-    'centerenergy_che.xml',
-    'centerenergy_che_private.xml',
-    'cp_che.xml'
-];
-rosreestr_outer.forEach(function (fName) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', `/Scripts/SAS/pkg/${fName}`, true);
-    xhr.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            let xml = new DOMParser().parseFromString(this.responseText, 'text/xml');
-            console.log(xml);
-            //console.log(xml.querySelectorAll(':scope > GeoObjectCollection > featureMembers > GeoObject'));
-            //xml.querySelectorAll(':scope > GeoObjectCollection > featureMembers > GeoObject').forEach(function (o) { });
-        }
-    };
-    xhr.send();
-});
+//rosreestr_layers.forEach(function (l) {
+//    l.cna = [];
+//    l.copyCNA = function () {
+//        copy(this.cna.join('\n'));
+//    };
+//    let xhr = new XMLHttpRequest();
+//    xhr.open('GET', `/Scripts/SAS/pkg/${l.name}.txt`, false);
+//    xhr.onreadystatechange = function () {
+//        let res = JSON.parse(this.response);
+//        res.features.forEach(function (f) {
+//            l.cna.push(`${f.properties.address.length}%sep%${f.properties.address}`);
+
+//        });
+//        console.log(res);
+//    };
+//    xhr.send();
+//});
